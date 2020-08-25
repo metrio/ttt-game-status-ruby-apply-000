9f4bd5040712db376544cd8f7d208b387b407e61
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-25 19:22:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combination|
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
+  index_1 = win_combination[0]
+  index_2 = win_combination[1]
+  index_3 = win_combination[2]
  
   position_1 = board[index_1] 
   position_2 = board[index_2] 
