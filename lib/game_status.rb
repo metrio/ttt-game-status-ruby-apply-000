5a027fc47e0ae0b0541b1ae1cafe11a5a6009528
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-25 19:17:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,5 +34,20 @@ def won?(board)
   
   return false 
   end
-end
   
+  def full?(board)
+    board.all? {|index| index == "X" || index == "O"}
+  end
+  
+  def draw?(board)
+    if !won?(board) && full?(board)
+      return true
+    else
+      return false
+    end
+  end
+  
+  def over?(board)
+    if won?(board) || full?(board) || draw?(board)
+    return true 
+    
