f602121df91df9a89074a1ee433c099cd711328f
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-25 18:02:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,11 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+# Define your WIN_COMBINATIONS constant
+#def WIN_COMBINATIONS[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+#end
+#end
+
 def won?(board, index)
   if board[top_row_win[0]] == "X" && board[top_row_win[1]]== "X" && board[top_row_win[2]] == "X"
     
@@ -10,7 +15,3 @@ def won?(board, index)
   end
 end
   
-# Define your WIN_COMBINATIONS constant
-#def WIN_COMBINATIONS[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-#end
-#end
