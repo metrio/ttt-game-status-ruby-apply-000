d460fbb95510a1b2abcdfd80991642970ab56aa3
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-25 17:51:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,5 +5,12 @@ end
 
 # Define your WIN_COMBINATIONS constant
 def WIN_COMBINATIONS
-  
+  top_row [0, 1, 2]
+  mid_row [3, 4, 5]
+  bot_row [6, 7, 8]
+  left_col [0, 3, 6]
+  mid_col [1, 4, 7]
+  right_col [2, 5, 8]
+  diag_1 [0, 4, 8]
+  diag_2 [2, 4, 6]
 end
